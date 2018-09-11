fbdbd1509d035e074be4e21bbcca29a3bc5a8ab1
Raat <ratt.np_92@live.com.ar>
2018-09-11 00:14:22 -0300

    Se crean los componentes Header y TarjetaViaje

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h3>Viajes</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    margin 20px
+
+</style>
---
+++ src/components/TarjetaViaje.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h3 class="inline">{{ nombre }}</h3>
+      <span class="inline">({{ carrera }})</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TarjetaViaje",
+  props: {
+    nombre: String,
+    carrera: String
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    margin-bottom 10px
+    cursor pointer
+
+  .card
+    display flex
+    flex-direction row
+    align-items center
+    padding 10px
+    box-shadow 0 5px 3px -3px rgba(0,0,0,.0)
+
+    transition .5s
+    &:hover
+      box-shadow 0 5px 3px -3px rgba(0,0,0,.3)
+
+  h3
+    margin 0 10px 0 0
+
+</style>
