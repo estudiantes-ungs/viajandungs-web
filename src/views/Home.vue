f2547ac7eff9a7edb676c514a2e4f6abc4dc2fc0
r42 <lna.dev@outlook.com>
2018-09-15 00:40:37 -0300

    Se implementa la conexión con la api, para mostrar los viajes creados

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,11 @@
   <div class="home">
     <Header />
     <div class="list-group">
-      <TarjetaViaje nombre="CACIC" carrera="Sistemas" />
-      <TarjetaViaje nombre="JAIIO" carrera="Sistemas" />
+      <TarjetaViaje v-for="(v, pk) in viajes" 
+        :nombre="v.name" 
+        carrera="Sistemas" 
+        :descripcion="v.description" 
+        :key="pk" />
     </div>
   </div>
 </template>
@@ -12,11 +15,26 @@
 import Header from "@/components/Header.vue";
 import TarjetaViaje from "@/components/TarjetaViaje.vue";
 
+const axios = require("axios");
+
 export default {
   name: "home",
   components: {
     Header,
     TarjetaViaje
+  },
+  data() {
+    return { viajes: [] };
+  },
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      axios.get("http://127.0.0.1:8000/viajeviajes").then(res => {
+        this.viajes = res.data;
+      });
+    }
   }
 };
 </script>
