8e48ee7f48c829c9dd83b62a07b502926b695ca3
r42 <lna.dev@outlook.com>
2018-09-15 00:43:46 -0300

    Se modifica el componente para mostrar los viajes en la vista de forma correcta (aunque no definitiva)

--- src/components/TarjetaViaje.vue
+++ src/components/TarjetaViaje.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="container">
     <div class="card">
-      <h3 class="inline">{{ nombre }}</h3>
-      <span class="inline">({{ carrera }})</span>
+      <div class="card-top">
+        <h3>{{ nombre }}</h3>
+        <span>({{ carrera }})</span>
+      </div>
+      <div class="card-description">
+        <span>{{ descripcion }}</span>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -12,7 +18,8 @@ export default {
   name: "TarjetaViaje",
   props: {
     nombre: String,
-    carrera: String
+    carrera: String,
+    descripcion: String
   }
 };
 </script>
@@ -24,14 +31,20 @@ export default {
 
   .card
     display flex
-    flex-direction row
-    align-items center
-    padding 10px
     box-shadow 0 5px 3px -3px rgba(0,0,0,.0)
-
     transition .5s
     &:hover
       box-shadow 0 5px 3px -3px rgba(0,0,0,.3)
+
+  .card-top
+    display flex
+    flex-direction row
+    align-items center
+    padding 10px
+
+  .card-description
+    margin 0 0 10px 10px
+    color #555
 
   h3
     margin 0 10px 0 0
