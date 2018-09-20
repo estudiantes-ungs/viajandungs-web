0948903a97a785aec0ba3f0e8dc70b5c74868c62
r42 <lna.dev@outlook.com>
2018-09-20 20:04:38 -0300

    Se crea el componente en el que se muestra la información detallada de un viaje específico

---
+++ src/components/DescripcionViaje.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container info">
+    <p>{{ descripcion }}</p>
+    <br />
+    <h5>Detalles:</h5>
+    <div>
+      <ul>
+        <li>Destino: {{ destino }}</li>
+        <li>Fecha de ida: {{ fechaIda }}</li>
+        <li>Fecha de regreso: </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    descripcion: String,
+    destino: String,
+    fechaIda: String
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .info
+    color #222
+
+  p
+    font-size 1.5em
+</style>
