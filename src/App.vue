c8b621dd256883eaf4fe2ffcd1940704a19d98d1
r42 <lna.dev@outlook.com>
2018-09-20 19:56:15 -0300

    Cambio 'Log in' por 'Iniciar sesión' ya que es más acorde al sistema

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav" class="navbar navbar-expand-lg navbar-dark bg-primary">
       <router-link to="/"><span class="navbar-brand">Viajandungs</span></router-link>
       <div class="nav navbar-nav">
-        <router-link to="/login" class="nav-item nav-link"><li>Log in</li></router-link>
+        <router-link to="/login" class="nav-item nav-link"><li>Iniciar Sesión</li></router-link>
       </div>
     </div>
     <router-view/>
