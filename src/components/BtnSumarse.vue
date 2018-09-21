6caf8965f3c46089adaf5351ce3960680db885c4
r42 <lna.dev@outlook.com>
2018-09-21 00:14:31 -0300

    Se crea el componente BtnSumarse. Un boton que permite al usuario sumarse a un viaje especifico. Aunque todavia sin funcionalidad

---
+++ src/components/BtnSumarse.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <button class="btn btn-primary btn-block">
+      Sumate!
+    </button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+  .btn-block
+    position relative
+    width 60%
+    left 50%
+    transform translate(-50%)
+</style>
