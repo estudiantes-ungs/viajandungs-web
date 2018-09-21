049952be2fd2a2332a6f9238b9fadcc9c87c0271
r42 <lna.dev@outlook.com>
2018-09-21 15:15:56 -0300

    Se implementan los componentes basicos para mostrar noticias en la vista de los viajes

---
+++ src/components/NoticiasViaje.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header title="Noticias" />
+    <div class="container">
+      <NoticiaViaje />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import NoticiaViaje from "@/components/NoticiaViaje.vue";
+
+export default {
+  components: {
+    Header,
+    NoticiaViaje
+  }
+};
+</script>
